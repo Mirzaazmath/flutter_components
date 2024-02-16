589cc7263cfec5b5c9e82efb51bbfac8b34a61a7
“Mirza” <mirza_azmathullah@cognerium.com>
2024-02-16 16:11:52 +0530

    customPopup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,75 +1,263 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/components/custom_dialog.dart';
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  SampleItem? selectedMenu;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("AlertDialog"),),
-      body: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: (){
-            _showMyDialog(context);
-          }, child:const Text("In-Build")),
-           Container(height: 20,),
-          ElevatedButton(onPressed: (){
-            _showCustomDialog(context);
-          }, child:const Text("Custom")),
+      appBar: AppBar(
+        title:const  Text("Custom PopUp Menu"),
+        actions: [
+          PopupMenuButton(
+              constraints: const BoxConstraints.expand(
+                  width: 300, height: 280),
+              initialValue: selectedMenu,
 
+              // Callback that sets the selected popup menu item.
+              onSelected: (SampleItem item) {
+                setState(() {
+                  selectedMenu = item;
+                });
+              },
+              itemBuilder: (BuildContext context) => [
+                PopupMenuItem(
+                    value: SampleItem.itemOne,
+                    child: StatefulBuilder(builder:
+                        (BuildContext context,
+                        StateSetter setState) {
+                      return Column(
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment
+                                      .start,
+                                  children: [
+                                    const Text(
+                                      "Importance",
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight:
+                                          FontWeight
+                                              .bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      padding:
+                                      const EdgeInsets
+                                          .symmetric(
+                                          horizontal:
+                                          10),
+                                      width: double
+                                          .infinity,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color:
+                                              Colors
+                                                  .grey),
+                                          color:
+                                          Colors
+                                              .white,
+                                          borderRadius:
+                                          BorderRadius
+                                              .circular(
+                                              10)),
+                                      height: 50,
+                                      child: TextFormField(),)
+
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment
+                                      .start,
+                                  children: [
+                                    const Text(
+                                      "Stage",
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight:
+                                          FontWeight
+                                              .bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      padding:
+                                      const EdgeInsets
+                                          .symmetric(
+                                          horizontal:
+                                          10),
+                                      width:
+                                      double.infinity,
+                                      decoration: BoxDecoration(
+                                          color: Colors
+                                              .white,
+                                          border: Border.all(
+                                              color: Colors
+                                                  .grey),
+                                          borderRadius:
+                                          BorderRadius
+                                              .circular(
+                                              10)),
+                                      height: 50,
+
+                                      child: TextFormField(),)
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment
+                                      .start,
+                                  children: [
+                                    const Text(
+                                      "Status",
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight:
+                                          FontWeight
+                                              .bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      padding:
+                                      const EdgeInsets
+                                          .symmetric(
+                                          horizontal:
+                                          10),
+                                      width: double
+                                          .infinity,
+                                      decoration: BoxDecoration(
+                                          color:
+                                          Colors
+                                              .white,
+                                          border: Border.all(
+                                              color:
+                                              Colors
+                                                  .grey),
+                                          borderRadius:
+                                          BorderRadius
+                                              .circular(
+                                              10)),
+                                      height: 50,
+
+                                      child: TextFormField(),)
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment
+                                      .start,
+                                  children: [
+                                    const Text(
+                                      "Assigned Truck",
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight:
+                                          FontWeight
+                                              .bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      padding:
+                                      const EdgeInsets
+                                          .symmetric(
+                                          horizontal:
+                                          10),
+                                      width: double
+                                          .infinity,
+                                      decoration: BoxDecoration(
+                                          color:
+                                          Colors
+                                              .white,
+                                          border: Border.all(
+                                              color:
+                                              Colors
+                                                  .grey),
+                                          borderRadius:
+                                          BorderRadius
+                                              .circular(
+                                              10)),
+                                      height: 50,
+                                      child: TextFormField(),)
+                                  ],
+                                ),
+                              ),
+
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(
+                                  top: 25),
+                              height: 50,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context)
+                                      .primaryColor,
+                                  borderRadius:
+                                  BorderRadius.circular(
+                                      10)),
+                              alignment: Alignment.center,
+                              child: const Text(
+                                "Apply",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight:
+                                    FontWeight.bold,
+                                    fontSize: 16),
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    })),
+              ] )
         ],
-
       ),
 
     );
   }
-  //// In Build Method////
-  Future<void> _showMyDialog(context) async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('This is a demo alert dialog.'),
-                Text('Would you like to approve of this message?'),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Approve'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-  ////// for Custom Dialog//////
-  _showCustomDialog(context){
-    showDialog(context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context){
-          return Dialog(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-              child: const  AddTruckDialogBox()
-          );
-
-
-        }
-    );
-
-  }
 }
+enum SampleItem { itemOne, itemTwo, itemThree }
